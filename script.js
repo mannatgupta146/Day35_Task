30ee15db297a34272612e8374acf0a067e8477ae
mannatgupta146 <mannatgupta146@gmail.com>
2025-11-07 14:21:10 +0530

    done

--- script.js
+++ script.js
@@ -2,15 +2,5 @@
 
 
 
-// 17. Print only first 3 odd numbers from 1 to 20
-// Use loop. Stop with break after 3 odd prints.
 
 
-// 18. Ask user 5 numbers. Count how many are positive
-// Use loop + condition + counter.
-
-
-// 19. ATM Simulator – Allow 3 withdrawals
-// Start with ₹1000 balance. Ask withdrawal amount 3 times.
-// If enough balance → deduct
-// Else → print “Insufficient balance”
